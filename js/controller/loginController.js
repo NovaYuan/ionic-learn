a3acb557ac8b577d30b6abb4a83a4753c0875a3d
NovaYuan <qiujuan_yuan@163.com>
2015-11-17 00:09:39 +0800

    引入新页面

---
+++ js/controller/loginController.js
@@ -0,0 +1,7 @@
+app.controller('loginCtrl', function($scope){
+        $scope.user = {
+                username: "yuanqiuuan",
+                password: '111111',
+                portrait: "##"
+        }
+});
